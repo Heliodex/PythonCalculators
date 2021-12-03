56c4b38267078a4a8487238f96472593f07a48c3
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2021-12-03 01:25:20 +0000

    new program style

--- interestCalculatorNew.py
+++ interestCalculatorNew.py
@@ -1,5 +1,5 @@
-# Heliodex 2021/08/24 calc
-# Last edited 2021/09/02 calc
+# Heliodex 2021/08/24
+# Last edited 2021/12/01 -- new program style
 # edit of vatRemover
 # uses Short Method, experimental
 
@@ -9,11 +9,14 @@ while True:
   val = float(input("Current value? "))
   int = float(input("Interest %? "))
   times = float(input("Number of times? "))
+  print(" ")
   
-  print("Values, ")
+  print("Values:")
   for i in range(times+1):
     f = (((100 + int)/100) ** i) * val
     print(f)
     # i, not times    
     
-  print("Total interest, " + str(f - val))
+  print("Total interest:")
+  print(f - val)
+  print(" ")
