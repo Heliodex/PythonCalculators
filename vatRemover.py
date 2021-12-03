69ac2ff339c89b0ff394a879737c8de2d5777415
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2021-12-03 01:29:11 +0000

    new program style

--- vatRemover.py
+++ vatRemover.py
@@ -1,24 +1,27 @@
-# Heliodex 2020/10/08 calc
-# Last edited 2021/08/24 calc
-# i wrote this during maths class lmao
+# Heliodex 2020/10/08
+# Last edited 2021/12/03 -- new program style
+# first program i wrote during maths class lmao
 
 print("Calculates original price based off current price and discount")
 
 while True:
+  print(" ")
   c = input("1 for add, 2 for subtract ")
   if (c == "1"):
     val = float(input("Current value? " ))
     per = float(input("Percentage added? " ))
-    
-    print("Original price, " + str((val/(per+100))*100))
+    print(" ")
+    print("Original price:")
+    print((val/(per+100))*100)
     # THIS has given incorrect answers and I didn't notice until today. 2021/08/24
     
   elif (c == "2"):
     val = float(input("Current value? " ))
     per = float(input("Percentage off? " ))
+    print(" ")
+    print("Original price:")
+    print(val * (1 + per/100))
     
-    print("Original price, " + str(val * (1 + per/100)))
-  
   else:
     break
   
