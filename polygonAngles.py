3e8f12d024f61383309c499e6443c91929b06b5f
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2021-12-03 01:26:11 +0000

    new program style

--- polygonAngles.py
+++ polygonAngles.py
@@ -1,14 +1,13 @@
 # Heliodex 2021/10/07
-# Last edited 2021/10/11
-# im putting these on github! woo
+# Last edited 2021/12/01 -- new program style
 
 print("Finds internal angles of polygons")
 
 while True:
   a = float(input("Number of sides? ")) # ...float?
-  intangle = 180*(a-2)/a
-  intangles = intangle * a
-  
-  print("Internal angle, " + str(intangle))
-  print("Total internal angle, " + str(intangles))
-  
+  print(" ")
+  print("Internal angle:") 
+  print((180*(a-2))/a)
+  print("Total internal angle:") 
+  print(180*(a-2))
+  print(" ")
