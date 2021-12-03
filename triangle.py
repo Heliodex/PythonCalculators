72be7bab360eb9ea941480e451ede6984e3b1e05
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2021-12-03 01:28:31 +0000

    new program style

--- triangle.py
+++ triangle.py
@@ -1,19 +1,26 @@
-# Heliodex 2020/11/03 calc
-# Last edited 2021/08/20 calc
+# Heliodex 2020/11/03
+# Last edited 2021/12/03 -- new program style
 
 print("Calculates area and perimiter of a triangle")
 
 while True:
+  print(" ")
   c = input("1 for area, 2 for perimiter ")
+  
   if (c == "1"):
-    ph = float(input("height? "))
-    pb = float(input("breadth? "))
-    print("area, " + str(ph*pb/2))
+    ph = float(input("Height? "))
+    pb = float(input("Breadth? "))
+    print(" ")
+    print("Area:")
+    print(ph*pb/2)
     
   elif (c == "2"):
-    px = float(input("side 1? "))
-    py = float(input("side 2? "))
-    pz = float(input("side 3? "))
-    print("perimiter, " + str(px+py+pz))
+    px = float(input("Side 1? "))
+    py = float(input("Side 2? "))
+    pz = float(input("Side 3? "))
+    print(" ")
+    print("Perimiter:")
+    print(px+py+pz)
+    
   else:
     break
