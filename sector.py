0590185a2aff104caa502256f7d3c0c730fc24af
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2021-12-03 01:27:47 +0000

    new program style

--- sector.py
+++ sector.py
@@ -1,5 +1,5 @@
 # Heliodex 2021/08/18
-# Last edited 2021/08/23
+# Last edited 2021/11/30 -- new program style
 
 from math import pi # no more "import *"
 print("Calculates area of a sector of a circle")
@@ -7,11 +7,19 @@ print("Calculates area of a sector of a circle")
 while True:
   deg = float(input("Degrees? "))
   rad = float(input("Radius? "))
-  f = deg/360 
+  print(" ")
+  f = deg/360
   
-  print("Sector 1, " + str(f*(pi*(rad**2))))
+  # 2021/11/30 new program style,
+  # makes it easier to select and see results
+  
+  print("Sector 1:")
+  print(f*(pi*(rad**2)))
   f = (360-deg)/360
   
-  print("Sector 2, " + str(f*(pi*(rad**2))))
+  print("Sector 2:")
+  print(f*(pi*(rad**2)))
   
-  print("Circumference, " + str(pi*(rad*2)))
+  print("Total area:")
+  print(pi*(rad**2))
+  print(" ")
