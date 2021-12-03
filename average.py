3cee034b536b76d719de4ee956692df10de92043
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2021-12-03 01:22:00 +0000

    new program style

--- average.py
+++ average.py
@@ -1,5 +1,5 @@
 # Heliodex 2021/08/24
-# Last editedw 2021/08/24
+# Last edited 2021/12/01 -- new program style
 
 print("Averages 2 numbers")
 
@@ -7,4 +7,6 @@ while True:
   a = float(input("Number 1? "))
   b = float(input("Number 2? "))
   
-  print("Average, " + str((a+b)/2))
+  print("Average:")
+  print((a+b)/2)
+  print(" ")
