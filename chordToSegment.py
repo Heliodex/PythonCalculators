da5c5b58adb061f47c7d19ce1115af0b16e37e28
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2021-12-03 01:23:02 +0000

    new program style

--- chordToSegment.py
+++ chordToSegment.py
@@ -1,10 +1,14 @@
 # Heliodex 2021/09/22
-# Last edited 2021/09/22
+# Last edited 2021/12/01 -- new program style
+
 
 print("Calculates segment depth from chord and circle")
 
 while True:
   rad = float(input("Circle radius? "))
   chd = float(input("Chord length? "))
+  print(" ")
   
-  print("Depth, " + str(rad-((rad**2)-((chd/2)**2))**0.5))
+  print("Depth:")
+  print(rad-((rad**2)-((chd/2)**2))**0.5)
+  print(" ")
