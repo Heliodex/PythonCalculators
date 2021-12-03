11c5464f4a771982dc1e54c3a6ea98491e8c4e0e
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2021-12-03 01:29:35 +0000

    new program style

--- volume.py
+++ volume.py
@@ -1,6 +1,6 @@
-# Heliodex 2020/11/19 calc
-# Last edited 2021/08/20 calc
-# I come back to fix this program and I see "2020/12/12" then i see the code
+# Heliodex 2020/11/19
+# Last edited 2021/12/03 -- new program style
+# 2021/08/20 I come back to fix this program and I see "2020/12/12" then i see the code
 
 from math import pi
 print("Calculates the volume of various 3D shapes")
@@ -22,6 +22,7 @@ def pyr(w, h, l):
   return str((l*w)*(h/3))
 
 while True:
+  print(" ")
   print("1 for cylinder, 2 for tri prism,")
   c = input("3 for cone, 4 for pyramid ")
   # i DESPISE this, when the string for
@@ -30,24 +31,32 @@ while True:
   if (c == "1"):
     h =  float(input("Height? "))
     w =  float(input("Width? "))
-    print("Volume, " + str(cyl(h, w)))
+    print(" ")
+    print("Volume:") 
+    print(cyl(h, w))
   
   elif (c == "2"):
     b =  float(input("Base length? "))
     h =  float(input("Height? "))
     l =  float(input("Length? "))
-    print("Volume, " + str(tri(b, h, l)))
+    print(" ")
+    print("Volume:") 
+    print(tri(b, h, l))
   
   elif (c == "3"):
     r = float(input("Base radius? "))
     h = float(input("Height? "))
-    print("Volume, " + str(con(r, h)))
+    print(" ")
+    print("Volume:") 
+    print(con(r, h))
   
   elif (c == "4"):
     w = float(input("Width? "))
     h = float(input("Height? "))
     l = float(input("Length? "))
-    print("Volume, " + str(pyr(w, h, l)))
+    print(" ")
+    print("Volume:") 
+    print(pyr(w, h, l))
   
   else:
     break
