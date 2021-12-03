3fdcb43bed368f2f97c853f0697894d66fbc2466
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2021-12-03 01:28:06 +0000

    new program style

--- trapezium.py
+++ trapezium.py
@@ -1,11 +1,15 @@
-# Heliodex 2020/11/04 calc
-# Last edited 2021/08/20 calc
+# Heliodex 2020/11/04
+# Last edited 2021/12/03 -- new program style
 
 print("Calculates the area of trapezium")
 
 while True:
-  s1 = float(input("side 1? "))
-  s2 = float(input("side 2? "))
-  h = float(input("height? "))
+  s1 = float(input("Side 1? "))
+  s2 = float(input("Side 2? "))
+  h = float(input("Height? "))
   
-  print("area, " + str((s1 + s2) / 2 * h))
+  print("Area:")
+  print((s1 + s2) / 2 * h)
+  print(" ")
+  # sweet and simple. more programs should be like this
+  
