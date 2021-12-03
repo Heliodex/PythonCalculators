828b6b1a43afdaf49a84462fda006455430e3a74
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2021-12-03 01:24:00 +0000

    new program style

--- converseOfPythagoras.py
+++ converseOfPythagoras.py
@@ -1,19 +1,24 @@
-# Heliodex ? calc
-# Last edited 2021/08/19 calc
+# Heliodex <2021/08/18
+# Last edited 2021/12/01 -- new program style
 
-a = input("side1 ")
-b = input("side2 ")
-c = input("hypot ")
-a = float(a)
-b = float(b)
-c = float(c)
+print("Calculates whether a triangle is a right-angled triangle ")
 
-if ( ((a**2)+(b**2)) == (c**2) ):
-  print("yes")
-else:
-  print("no")
-
-print(a**2)
-print(b**2)
-print((a**2)+(b**2))
-print(c**2)
+while True: 
+  a = input("Side 1? ")
+  b = input("Side 2? ")
+  c = input("Hypoteneuse? ")
+  a = float(a)
+  b = float(b)
+  c = float(c)
+  print(" ")
+  
+  if ( ((a**2)+(b**2)) == (c**2) ):
+    print("Right-angled")
+  else:
+    print("Not right-angled")
+  
+  print(a**2)
+  print(b**2)
+  print((a**2)+(b**2))
+  print(c**2)
+  print(" ") # "new program style" didn't do much...
