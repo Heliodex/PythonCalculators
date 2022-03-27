2899cff55359fb34138671a4c4341fb0903297c8
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2022-03-27 23:42:38 +0100

    the program actually works now

--- radiusFromArc.py
+++ radiusFromArc.py
@@ -1,26 +1,20 @@
 # Heliodex 2021/08/23
-# Last edited 2021/12/03 -- new program style
+# Last edited 2022/03/27 -- make the program ACTUALLY WORK
 
-from math import pi
+from math import pi, radians
 print("Calculates the length of radius based on arc length")
 
 while True:
-  arc = float(input("Arc? "))
-  deg = float(input("Degrees? "))
-  f = deg/360 
-  print(" ")
-  
-  print("Arc 1:")
-  print(f*(pi*dia))
-  f = (360-deg)/360
-  
-  print("Arc 2:")
-  print(f*(pi*dia))
-  print(" ")
-  
-  print("Circumference")
-  print(pi*dia)
-  # the only part I don't gotta change
-  print(" ")
-  
-  
+    arc = float(input("Arc? "))
+    deg = float(input("Degrees? "))
+    f = radians(deg)
+    print(" ")
+
+    print("Radius:")
+    print(arc/f)
+    print(" ")
+
+    print("Circumference")
+    print((arc/f)*2*pi)  # a tau would be useful right now
+    # the only part I don't gotta change
+    print(" ")
