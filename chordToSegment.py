406d482d643bead7474a48f8cb0899775bf13018
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2022-03-27 23:01:07 +0100

    Calculate both possible depths

--- chordToSegment.py
+++ chordToSegment.py
@@ -1,14 +1,18 @@
 # Heliodex 2021/09/22
-# Last edited 2021/12/01 -- new program style
-
+# Last edited 2021/12/03 -- calculate second depth
 
 print("Calculates segment depth from chord and circle")
 
 while True:
-  rad = float(input("Circle radius? "))
-  chd = float(input("Chord length? "))
-  print(" ")
-  
-  print("Depth:")
-  print(rad-((rad**2)-((chd/2)**2))**0.5)
-  print(" ")
+    rad = float(input("Circle radius? "))
+    chd = float(input("Chord length? "))
+
+    segmDepth = rad-((rad**2)-((chd/2)**2))**0.5
+    # Least optimal equation ever
+    print(" ")
+
+    print("Depth 1:")
+    print(segmDepth)
+    print("Depth 2:")
+    print((rad*2)-segmDepth)
+    print(" ")
