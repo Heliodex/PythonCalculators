9b5994489f85ea6766efa6a9508cf3ba7f2d7b82
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2022-03-27 23:03:16 +0100

    1 space

--- angleFromArcOrSector.py
+++ angleFromArcOrSector.py
@@ -5,31 +5,31 @@ from math import pi
 print("Calculates angle of a sector from arc+radius")
 
 while True:
-  c = input("1 for with arc, 2 for with area ")
-  
-  if (c == "1"):
-    arc = float(input("Arc? "))
-    rad = float(input("Radius? "))
-    print(" ")
-    
-    print("Angle:")
-    print((arc/((rad*2)*pi))*360)
-    
-    print("Circumference:")
-    print(pi*(rad*2))
-    print(" ")
-  
-  elif (c == "2"): # ah yes. classic "ellif"
-    area = float(input("Area? "))
-    rad = float(input("Radius? "))
-    print(" ")
-    
-    print("Angle:")
-    print((area/(pi*(rad**2)))*360)
-    
-    print("Circumference:")
-    print(pi*(rad*2))
-    print(" ")
-  
-  else:
-    break
+    c = input("1 for with arc, 2 for with area ")
+
+    if (c == "1"):
+        arc = float(input("Arc? "))
+        rad = float(input("Radius? "))
+        print(" ")
+
+        print("Angle:")
+        print((arc/((rad*2)*pi))*360)
+
+        print("Circumference:")
+        print(pi*(rad*2))
+        print(" ")
+
+    elif (c == "2"):  # ah yes. classic "ellif"
+        area = float(input("Area? "))
+        rad = float(input("Radius? "))
+        print(" ")
+
+        print("Angle:")
+        print((area/(pi*(rad**2)))*360)
+
+        print("Circumference:")
+        print(pi*(rad*2))
+        print(" ")
+
+    else:
+        break
