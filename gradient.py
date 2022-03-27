1213f4a4d59dcfeb9045060e0d73ca5a911e0206
Lewin Kelly <50165230+Heliodex@users.noreply.github.com>
2022-03-27 23:06:20 +0100

    add gradient.py

---
+++ gradient.py
@@ -0,0 +1,15 @@
+# Heliodex 2022/01/18
+# Last edited 2022/01/24 -- forgot new program style
+
+print("Calculates the gradient of a straight line given 2 points")
+
+while True:
+    x1 = float(input("X of point 1? "))
+    y1 = float(input("Y of point 1? "))
+    x2 = float(input("X of point 2? "))
+    y2 = float(input("Y of point 2? "))
+
+    print(" ")
+    print("Gradient:")
+    print((y2-y1)/(x2-x1))
+    print(" ")
